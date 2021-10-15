e7138f0ae60f587a96c7c48fe745be29c0080460
juhye6v6 <111601joo@naver.com>
2021-10-16 00:54:17 +0900

    좋아요 기능개선1

---
+++ src/ui/Review.java
@@ -0,0 +1,33 @@
+package ui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Review {
+	
+	
+	
+	//db 연결
+		public static Connection makeConnection() {
+			String url="jdbc:mysql://localhost/menu1";
+			String id="root";
+			String password="rhksflwk123!";
+			Connection con=null;
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("드라이버 적재 성공");
+				con=DriverManager.getConnection(url,id,password);
+				System.out.println("데이터베이스 연결 성공");
+			}catch (ClassNotFoundException e) {
+				System.out.println("드라이버를 찾을 수 없습니다");
+			}catch (SQLException e) {
+				System.out.println("연결에 실패하였습니다.");
+			}return con;
+		}
+		
+		public static void main(String[] args) {
+			Review review=new Review();
+		}
+
+}
